379825bbb83a1ff42ad4b5e02177c6d77ce5e57a
davichodavid <davichodavid@yahoo.com>
2019-09-13 17:35:18 -0400

    login complete, ready to create backend login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react';
+import VERIFY_USER from '../Events';
 
 export default class LoginForm extends Component {
-  render(props) {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nickname: '',
+      error: ''
+    };
+  }
+
+  setUser = ({ user, isUser }) => {
+    if (isUser) {
+      this.setError('Username taken');
+    } else {
+      this.props.setUser(user);
+    }
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { socket } = this.props;
+    const { nickname } = this.state;
+    socket.emit(VERIFY_USER, nickname, this.setUser);
+  };
+
+  handleChange = event => {
+    this.setState({ nickname: event.target.value });
+  };
+
+  setError = error => {
+    this.setState({ error });
+  };
+
+  render() {
+    const { nickname, error } = this.state;
     return (
       <div className='login'>
         <form onSubmit={this.handleSubmit} className='login-form'>
@@ -14,8 +48,9 @@ export default class LoginForm extends Component {
             id='nickname'
             value={nickname}
             onChange={this.handleChange}
-            placeholder={'MyCoolUsername'}
+            placeholder={'Enter Your Username'}
           />
+          <div className='error'>{error ? error : null}</div>
         </form>
       </div>
     );
